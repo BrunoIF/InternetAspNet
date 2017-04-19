60de24cbe353505ef46ae3b65e926f27a1785718
logonrm <logonrm@fiap.com.br>
2017-04-19 08:48:53 -0300

    Busca CEP - Aula 04

---
+++ Aula04/BuscaCep.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="BuscaCep.aspx.cs" Inherits="BuscaCep" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <h3>Buscar CEP</h3>
+        Informe o CEP:
+        <asp:TextBox ID="txtCep" runat="server"></asp:TextBox>
+        &nbsp;<asp:Button ID="btnCep" runat="server" Text="Buscar CEP" OnClick="btnCep_Click" />
+        <br />
+        <br />
+        Logradouro: <asp:TextBox ID="txtLogradouro" runat="server"></asp:TextBox>
+&nbsp; Número:
+        <asp:TextBox ID="txtNumero" runat="server"></asp:TextBox>
+&nbsp;Complemento:
+        <asp:TextBox ID="txtComplemento" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        Bairro: <asp:TextBox ID="txtBairro" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        Cidade:
+        <asp:TextBox ID="txtCidade" runat="server"></asp:TextBox>
+&nbsp;Estado:
+        <asp:TextBox ID="txtUF" runat="server"></asp:TextBox>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Aula04/BuscaCep.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+public partial class BuscaCep : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnCep_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            string rua;
+            DataSet ds = new DataSet();
+            string endereco = "http://cep.republicavirtual.com.br/web_cep.php?cep=@cep&formato=xml".Replace("@cep",txtCep.Text);
+            ds.ReadXml(endereco);
+            rua = ds.Tables[0].Rows[0]["logradouro"].ToString();
+
+            if (rua == "")
+            {
+                Response.Write("<script>alert('CEP Inválido.')</script>");
+                txtCep.Focus();
+            }
+            else
+            {
+                txtLogradouro.Text = ds.Tables[0].Rows[0]["tipo_logradouro"].ToString() + rua;
+                txtBairro.Text = ds.Tables[0].Rows[0]["bairro"].ToString();
+                txtCidade.Text = ds.Tables[0].Rows[0]["cidade"].ToString();
+                txtUF.Text = ds.Tables[0].Rows[0]["uf"].ToString();
+                txtNumero.Focus();
+            }
+        }
+        catch (Exception)
+        {
+            Response.Write("<script>alert('Serviço Indisponível...tente novamente')</script>");
+        }
+    }
+}
