7e7c56945b2f623f6a13874041e5c787e9455bc3
logonrm <logonrm@fiap.com.br>
2017-04-26 08:27:19 -0300

    Criação da tb_usuario

---
+++ Aula05/db_usuario_Query01.sql
@@ -0,0 +1,22 @@
+create table tb_usuario_02(
+	Id_usuario int primary key identity(1,1),
+	Nome_usuario nvarchar(50) not null,
+	email_usuario nvarchar(50) not null,
+	tel_usuario nvarchar(20) not null
+)
+
+sp_help tb_usuario_02
+
+insert into tb_usuario_02 values
+('Israel','icajaijr@gmail.com','123456789')
+
+insert into tb_usuario_02 values
+('José','josegames@gmail.com','987654321')
+
+insert into tb_usuario_02 values
+('Carlos','carlosseg@gmail.com','456321789')
+
+insert into tb_usuario_02 values
+('Mábio','mabiofing@gmail.com','321654789')
+
+select * from tb_usuario_02
