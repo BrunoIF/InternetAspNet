33881a6c6e7d29fe6ecb838d93d5e27cc474bac2
logonrm <logonrm@fiap.com.br>
2017-08-23 09:56:44 -0300

    Projeto CRUD em Camadas, Criação de Classe Pessoa, Criação de Classe Conexao

---
+++ Aula09/CRUD - Camadas/CRUD/DAL/Persistence/Conexao.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace DAL.Persistence
+{
+    public class Conexao
+    {
+        // Atributos
+        // Protected -> apenas classes que herdam de Conexao terão acesso
+        protected SqlConnection con;
+        protected SqlCommand cmd;
+        protected SqlDataReader dr; // Tratar os registros obtidos nas queries
+    }
+
+    // Método para acessar a tabela
+
+
+    // Método para fechar a tabela
+}
